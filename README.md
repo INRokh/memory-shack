cc4b7c70672891dc5dbe7aababfc5fd614d44ac9
Nina <ilnarnika@gmail.com>
2019-12-03 11:34:19 +1100

    OCR text in images.

--- README.md
+++ README.md
@@ -3,3 +3,84 @@
 ## Goal
 
 This is an application that extracts text from images and uses it as a search index.
+
+### Deployment to Google Cloud
+
+1. Create new project in GCP.
+1. Enable the Cloud Functions, Cloud Pub/Sub, Cloud Storage, Cloud Translation, and Cloud Vision APIs.
+1. Install Google Cloud SDK (on Mac OSX):
+
+```shell
+brew cask install google-cloud-sdk
+```
+
+4. Update gcloud components:
+
+```shell
+gcloud components update
+```
+
+5.  Install the latest version of Node.js:
+
+```shell
+nvm install stable
+```
+
+6.  Initialize gcloud:
+
+```shell
+gcloud init
+```
+
+7. Create a Cloud Storage bucket to upload your images, where YOUR_IMAGE_BUCKET_NAME is a globally unique bucket name:
+
+```shell
+gsutil mb gs://YOUR_IMAGE_BUCKET_NAME
+```
+
+8. Create a Cloud Storage bucket to save the result, where YOUR_TEXT_BUCKET_NAME is a globally unique bucket name:
+
+```shell
+gsutil mb gs://YOUR_TEXT_BUCKET_NAME
+```
+
+9. Clone this repository and cd into it:
+
+```shell
+git clone https://github.com/INRokh/memory-shack
+cd memory-shack
+```
+
+10. Create a config.json file in the app directory with the following contents:
+
+```shell
+{
+    "RESULT_TOPIC": "YOUR_RESULT_TOPIC_NAME",
+    "RESULT_BUCKET": "YOUR_TEXT_BUCKET_NAME"
+}
+```
+
+Replace YOUR_RESULT_TOPIC_NAME with a Pub/Sub topic name to be used for saving results.
+
+Replace YOUR_TEXT_BUCKET_NAME with a bucket name used for saving text results.
+
+### Uploading an image
+
+1. Upload an image to your image Cloud Storage bucket:
+
+```shell
+gsutil cp PATH_TO_IMAGE gs://YOUR_IMAGE_BUCKET_NAME
+```
+
+- PATH_TO_IMAGE is a path to an image file (that contains text) on your local system.
+- YOUR_IMAGE_BUCKET_NAME is the name of the bucket where you are uploading images.
+
+2. Watch the logs to be sure the executions have completed:
+
+```shell
+gcloud functions logs read --limit 100
+```
+
+
+
+
---
+++ index.js
@@ -0,0 +1,130 @@
+'use strict';
+
+const config = require('nconf')
+  .env()
+  .file('./config.json');
+
+// Get a reference to the Pub/Sub component
+const {PubSub} = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+// Get a reference to the Cloud Storage component
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+
+// const gcs = require('@google-cloud/storage');
+// const storage = new gcs.Storage();
+
+// Get a reference to the Cloud Vision API component
+const Vision = require('@google-cloud/vision');
+const vision = new Vision.ImageAnnotatorClient();
+
+/**
+ * Publishes the result to the given pubsub topic and returns a Promise.
+ *
+ * @param {string} topicName Name of the topic on which to publish.
+ * @param {object} data The message data to publish.
+ */
+
+const publishResult = async (topicName, data) => {
+  const dataBuffer = Buffer.from(JSON.stringify(data));
+
+  const [topic] = await pubsub.topic(topicName).get({autoCreate: true});
+  topic.publish(dataBuffer);
+};
+
+/**
+ * Detects the text in an image using the Google Vision API.
+ *
+ * @param {string} bucketName Cloud Storage bucket name.
+ * @param {string} filename Cloud Storage file name.
+ * @returns {Promise}
+ */
+const detectText = async (bucketName, filename) => {
+  console.log(`Looking for text in image ${filename}`);
+  const [textDetections] = await vision.textDetection(
+    `gs://${bucketName}/${filename}`
+  );
+  const [annotation] = textDetections.textAnnotations;
+  const text = annotation ? annotation.description : '';
+  console.log(`Extracted text from image:`, text);
+
+  const messageData = {
+    text: text,
+    filename: filename,
+  };
+
+  await publishResult(config.get('RESULT_TOPIC'), messageData);
+};
+
+/**
+ * Appends a .txt suffix to the image name.
+ *
+ * @param {string} filename Name of a file.
+ * @returns {string} The new filename.
+ */
+const renameImageForSave = (filename) => {
+  return `${filename}.txt`;
+};
+
+/**
+ * This function is exported by index.js, and is executed when
+ * a file is uploaded to the Cloud Storage bucket you created
+ * for uploading images.
+ *
+ * @param {object} event A Google Cloud Storage File object.
+ */
+exports.processImage = async event => {
+  const {bucket, name} = event;
+
+  if (!bucket) {
+    throw new Error(
+      'Bucket not provided. Make sure you have a "bucket" property in your request'
+    );
+  }
+  if (!name) {
+    throw new Error(
+      'Filename not provided. Make sure you have a "name" property in your request'
+    );
+  }
+
+  await detectText(bucket, name);
+  console.log(`File ${name} processed.`);
+};
+
+/**
+ * This function is exported by index.js, and is executed when
+ * a message is published to the Cloud Pub/Sub topic specified
+ * by the RESULT_TOPIC value in the config.json file. The
+ * function saves the data packet to a file in GCS.
+ *
+ * @param {object} event The Cloud Pub/Sub Message object.
+ * @param {string} {messageObject}.data The "data" property of the Cloud Pub/Sub
+ * Message. This property will be a base64-encoded string that you must decode.
+ */
+exports.saveResult = async event => {
+  const pubsubData = event.data;
+  const jsonStr = Buffer.from(pubsubData, 'base64').toString();
+  const {text, filename} = JSON.parse(jsonStr);
+
+  if (!text) {
+    throw new Error(
+      'Text not provided. Make sure you have a "text" property in your request'
+    );
+  }
+  if (!filename) {
+    throw new Error(
+      'Filename not provided. Make sure you have a "filename" property in your request'
+    );
+  }
+
+  console.log(`Received request to save file ${filename}`);
+
+  const bucketName = config.get('RESULT_BUCKET');
+  const newFilename = renameImageForSave(filename);
+  const file = storage.bucket(bucketName).file(newFilename);
+
+  console.log(`Saving result to ${newFilename} in bucket ${bucketName}`);
+
+  await file.save(text);
+  console.log(`File saved.`);
+};
